c947e75d5f9ae5af8bcd6c267be55cd4294e9350
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-22 15:56:06 +0200

    first commit

---
+++ src/main/java/com/example/castells_casteller/CastellsCastellerApplication.java
@@ -0,0 +1,13 @@
+package com.example.castells_casteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastellsCastellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastellsCastellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/castells_casteller/CastellsCastellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.castells_casteller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastellsCastellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
